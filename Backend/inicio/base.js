4d1f4ce71f674198b503f8a7a499f4a0c1351efa
ThiagoMolon <thiago.molon03@gmail.com>
2026-06-02 11:47:32 -0300

    inicio 2 tri

---
+++ Backend/inicio/base.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const local = 'http://localhost:3000';
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write('Bem-vindo ao meu servidor!');
+    } else if (req.url === '/sobre') {
+        res.write('Esta é a página sobre nós.');
+    } else {
+        res.write('Página não encontrada.');
+    }
+    res.end();
+})
+console.log(`Servidor rodando em ${local}`);
+server.listen(3000);
